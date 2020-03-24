765ee9607dc5618d93a30e63a339a409bc374227
Daeseong7966 <41766637+Daeseong7966@users.noreply.github.com>
2020-03-24 22:25:21 +0900

    [Fix, Add]
    add filter function

--- GalaxyInterior/screens/Q&AScreen.js
+++ GalaxyInterior/screens/Q&AScreen.js
@@ -4,12 +4,24 @@ import axios from 'axios';
 import QAcomponent from '../components/Q&Acomponent';
 import MaterialCMIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import { DotIndicator } from 'react-native-indicators';
+import * as Font from 'expo-font';
 
 class QAScreen extends React.Component{
     state = {
         QAdata : [],
         SearchTitle : true, //true = 제목, false = 작성자
-        FilterKeyword : ""
+        FilterKeyword : "",
+        isLockFilter : false, //잠금이 되어있는지 안되어있는지 체크하는 필터
+        fontLoad : false,
+    }
+
+    async componentDidMount(){
+        await Font.loadAsync({
+            'SCDream_M' : require('../assets/fonts/SCDream_M.otf'),
+        });
+        this.setState({fontLoad : true});
+        this.fetchQA();
     }
 
     fetchQA(){
@@ -17,7 +29,8 @@ class QAScreen extends React.Component{
         .then((response) => {
             console.log(response.data);
             this.setState({
-                QAdata : response.data
+                QAdata : response.data,
+                FilterKeyword : ""
             });
         })
         .catch((error) => {
@@ -29,32 +42,66 @@ class QAScreen extends React.Component{
         this.setState({SearchTitle : !this.state.SearchTitle});
     }
 
-    componentDidMount(){
-        this.fetchQA();
+    toggleisLockFilter(){
+        this.setState({isLockFilter : !this.state.isLockFilter});
     }
 
     render(){
-        return(
-            <View style = {styles.container}>
-                <View style = {styles.TopContainer}>
-                    <View style = {styles.SerchBox}>
-                        <TextInput 
-                            style = {styles.SerchTextInput}
-                            placeholder = "검색어를 입력해주세요."
-                            onChangeText = {(Text) => this.setState({FilterKeyword : Text})}
-                        />
-                        <TouchableOpacity onPress = {() => this.toggleSearchTitle()}>
-                            {
-                                this.state.SearchTitle ? <MaterialCMIcon name = "subtitles-outline" size = {30} color = "black" /> : <FontAwesome5Icon name = "user-alt" size = {25} color = "black" />
-                            }
-                        </TouchableOpacity>
+        const filteredComponents = (data) => {
+            if(!this.state.isLockFilter){
+                data = data.filter((qadata) => {
+                    if(this.state.SearchTitle){
+                        return qadata.title.indexOf(this.state.FilterKeyword) > -1; //비밀글이 아닐 때 제목으로 검색하면 결과값 전달
+                    }else{
+                        return qadata.author.indexOf(this.state.FilterKeyword) > -1; //비밀글이 아닐 때 작성자로 검색하면 결과값 전달
+                    }
+                });
+                return data.map((qadata) => {
+                    return <QAcomponent QAdata = {qadata} key = {qadata.id} />
+                })
+            }else{
+                data = data.filter((qadata) => {
+                    if(this.state.SearchTitle){
+                        return !qadata.isLock && qadata.title.indexOf(this.state.FilterKeyword) > -1; //비밀글이 아닌고, 제목 검색
+                    }else{
+                        return !qadata.isLock && qadata.author.indexOf(this.state.FilterKeyword) > -1; //비밀글이 아니고, 작성자 검색
+                    }
+                });
+                return data.map((qadata) => {
+                    return <QAcomponent QAdata = {qadata} key = {qadata.id} />
+                })
+            }
+        }
+        if(this.state.fontLoad){
+            return(
+                <View style = {styles.container}>
+                    <View style = {styles.TopContainer}>
+                        <View style = {styles.SerchBox}>
+                            <TextInput 
+                                style = {styles.SerchTextInput}
+                                placeholder = "검색어를 입력해주세요."
+                                onChangeText = {(Text) => this.setState({FilterKeyword : Text})}
+                            />
+                            <TouchableOpacity onPress = {() => this.toggleSearchTitle()}>
+                                {
+                                    this.state.SearchTitle ? <MaterialCMIcon name = "subtitles-outline" size = {30} color = "black" /> : <FontAwesome5Icon name = "user-alt" size = {25} color = "black" />
+                                }
+                            </TouchableOpacity>
+                        </View>
                     </View>
+
+                    <TouchableOpacity onPress = {() => this.toggleisLockFilter()} style = {{flexDirection : "row", alignItems : "center", alignSelf : "flex-start", marginLeft : "5%"}}>
+                        <FontAwesome5Icon name = "check" size = {15} color = {this.state.isLockFilter ? "#1C5AF5" : "#7B7B7B"} />
+                        <Text style = {[styles.FilterFont, this.state.isLockFilter ? styles.TrueFilterFont : styles.FalseFilterFont]}>비밀글 제외</Text>
+                    </TouchableOpacity>
+                    {
+                        this.state.QAdata ? filteredComponents(this.state.QAdata) : null
+                    }
                 </View>
-                {
-                    this.state.QAdata.map(QAdata => <QAcomponent QAdata = {QAdata} key = {QAdata.id} />)
-                }
-            </View>
-        );
+            );
+        }else{
+            return <View style = {{flex : 1,justifyContent : "center", alignItems : "center", backgroundColor : "#FCF9F0"}}><DotIndicator color = "#7B7B7B"/></View>
+        }
     }
 }
 
@@ -90,7 +137,18 @@ const styles = StyleSheet.create({
     SerchTextInput : {
         width : "90%",
         height : "100%",
-        paddingLeft : 10
+        paddingLeft : 10,
+        fontFamily : "SCDream_M"
+    },
+    FilterFont : {
+        fontFamily : "SCDream_M",
+        marginLeft : 5
+    },
+    TrueFilterFont : {
+        color : "black"
+    },
+    FalseFilterFont : {
+        color : "#7B7B7B"
     }
 })
 
